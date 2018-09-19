e1b7aefdd8bb0dff554d2c6806a86301da3008ea
Paul Knopf <pauldotknopf@gmail.com>
2018-09-18 23:00:02 -0400

    Working on CI.

--- build/scripts/Program.cs
+++ build/scripts/Program.cs
@@ -6,6 +6,8 @@ using static Build.Buildary.Runner;
 using static Build.Buildary.Directory;
 using static Build.Buildary.Path;
 using static Build.Buildary.Shell;
+using static Build.Buildary.Log;
+using static Build.Buildary.File;
 
 namespace Build
 {
@@ -14,15 +16,23 @@ namespace Build
         static Task Main(string[] args)
         {
             var options = ParseOptions(args);
-
-            Target("clean", () =>
+            var deployKey = ExpandPath("./deploy.key");
+            var tmpRepo = ExpandPath("./tmp-repo");
+            var output = ExpandPath("./output");
+            var commitAuthorEmail = Environment.GetEnvironmentVariable("COMMIT_AUTHOR_EMAIL");
+            var sha = ReadShell("git rev-parse --short HEAD");
+            
+            Target("build-clean", () =>
             {
-                CleanDirectory(ExpandPath("./output"));
+                if (DirectoryExists(output))
+                {
+                    DeleteDirectory(output);
+                }
             });
             
-            Target("build", DependsOn("clean"), () =>
+            Target("build", DependsOn("build-clean"), () =>
             {
-                RunShell("dotnet run --project statik-project-doc/StatikProject/StatikProject.csproj build");
+                RunShell($"dotnet run --project statik-project-doc/StatikProject/StatikProject.csproj build -o {output}");
             });
             
             Target("serve", () =>
@@ -30,8 +40,73 @@ namespace Build
                 RunShell("dotnet run --project statik-project-doc/StatikProject/StatikProject.csproj serve");
             });
             
-            Target("default", DependsOn("serve"));
+            Target("deploy-clean", () =>
+            {
+                if (DirectoryExists(tmpRepo))
+                {
+                    DeleteDirectory(tmpRepo);
+                }
+            });
+            
+            Target("deploy", DependsOn("deploy-clean"), () =>
+            {
+                if (Travis.IsTravis)
+                {
+                    if (Travis.IsPullRequest)
+                    {
+                        Info("Pull request, skipping deploy...");
+                        return;
+                    }
+                
+                    var branch = Travis.Branch;
+                    if (branch != "staging")
+                    {
+                        Info("Not on staging branch, skipping deploy...");
+                        return;
+                    }
+                }
 
+                if (FileExists(ExpandPath(deployKey)))
+                {
+                    Info($"Adding {deployKey} to ssh-agent...");
+                    RunShell($"chmod 600 {ExpandPath(deployKey)} && eval `ssh-agent -s` && ssh-add ${ExpandPath(deployKey)}");
+                }
+                else
+                {
+                    Info($"Key {deployKey} doesn't exist, skipping ssh-agent...");
+                }
+                
+                if (string.IsNullOrEmpty(commitAuthorEmail))
+                {
+                    Failure("No COMMIT_AUTHOR_EMAIL, skipping deploy...");
+                    Environment.Exit(1);
+                }
+                
+                RunShell($"git clone git@github.com:qmlnet/qmlnet.github.io.git {tmpRepo}");
+                
+                using (ChangeDirectory(tmpRepo))
+                {
+                    RunShell("git checkout master || git checkout --orphan master");
+
+                    RunShell("git rm -r .");
+                    RunShell($"cp -r {output}/. {tmpRepo}");
+                    RunShell("git add .");
+
+                    if (string.IsNullOrEmpty(ReadShell("git status --porcelain")))
+                    {
+                        Info("No changes, skipping deploy...");
+                        return;
+                    }
+                    
+                    RunShell("git config user.name \"Travis CI\"");
+                    RunShell($"git config user.email \"{commitAuthorEmail}\"");
+                    RunShell($"git commit -m \"Deploy to GitHub Pages: {sha}\"");
+                }
+            });
+            
+            Target("default", DependsOn("serve"));
+            Target("ci", DependsOn("build", "deploy"));
+            
             return Run(options);
         }
     }
